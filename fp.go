e6fa6ee144a5e5f049c1a53f09afd5e0d9c555a1
ayahav <ayahav@outbrain.com>
2022-08-13 22:06:02 +0300

    added I functions

--- fp.go
+++ fp.go
@@ -4,13 +4,30 @@ package fp
 //
 // Example:
 //		s := []int{1, 2, 3, 4}
-//		cb := func(elem int, index int) int {
+//		cb := func(elem int) int {
 //			return elem * 2
 //		}
 //
 //		ns := Map(cb, s) // output: ns = [2, 4, 6, 8]
 //
-func Map[T any, Z any](callback func(T, int) Z, s []T) []Z {
+func Map[T any, Z any](callback func(T) Z, s []T) []Z {
+	var newSlice []Z
+	for _, elem := range s {
+		newSlice = append(newSlice, callback(elem))
+	}
+
+	return newSlice
+}
+
+// MapI is same as Map except from the callback function which also receives the index of the current element in the slice.
+// Example:
+//		s := []int{1, 2, 3, 4}
+// 		cb := func(elem int, index int) int {
+//			return index * elem
+//		}
+//
+//		ns = MapI(cb, s) // output: ns = [0, 2, 6, 12]
+func MapI[T any, Z any](callback func(T, int) Z, s []T) []Z {
 	var newSlice []Z
 	for i, elem := range s {
 		newSlice = append(newSlice, callback(elem, i))
@@ -24,12 +41,32 @@ func Map[T any, Z any](callback func(T, int) Z, s []T) []Z {
 //
 // Example:
 // 		s := []int{1, 3, 5, 4}
+//		cb := func(elem int) bool {
+//			return elem%2 == 0
+//		}
+//
+//		ns := Filter(cb, s) // output: ns = [4]
+func Filter[T any](pred func(T) bool, s []T) []T {
+	var newSlice []T
+	for _, elem := range s {
+		if pred(elem) {
+			newSlice = append(newSlice, elem)
+		}
+	}
+
+	return newSlice
+}
+
+// FilterI is same as Filter except from the predicate function which also receives the index of the current element in the slice.
+//
+// Example:
+// 		s := []int{1, 3, 5, 4}
 //		cb := func(elem int, index int) bool {
 //			return elem%2 == 0 || index%2 != 0
 //		}
 //
-//		ns := Filter(cb, s) // output: ns = [3, 4]
-func Filter[T any](pred func(T, int) bool, s []T) []T {
+//		ns := FilterI(cb, s) // output: ns = [3, 4]
+func FilterI[T any](pred func(T, int) bool, s []T) []T {
 	var newSlice []T
 	for i, elem := range s {
 		if pred(elem, i) {
@@ -51,7 +88,24 @@ func Filter[T any](pred func(T, int) bool, s []T) []T {
 //		}
 //
 //		ns := Reduce(cb, 0, s) // output: ns = 10
-func Reduce[T any](reducer func(T, T, int) T, acc T, s []T) T {
+func Reduce[T any](reducer func(T, T) T, acc T, s []T) T {
+	for _, elem := range s {
+		acc = reducer(acc, elem)
+	}
+
+	return acc
+}
+
+// ReduceI is same as Reduce except from the reducer function which also receives the index of the current element in the slice.
+//
+// Example:
+// 		s := []int{1, 2, 3, 4}
+//		cb := func(acc int, curr int, index int) int {
+//			return acc + curr + index
+//		}
+//
+//		ns := ReduceI(cb, 0, s) // output: ns = 16
+func ReduceI[T any](reducer func(T, T, int) T, acc T, s []T) T {
 	for i, elem := range s {
 		acc = reducer(acc, elem, i)
 	}
--- fp_test.go
+++ fp_test.go
@@ -14,10 +14,10 @@ func TestFp(t *testing.T) {
 		Age  int
 	}
 
-	t.Run("Map", func(t *testing.T) {
+	t.Run("Map,MapI", func(t *testing.T) {
 
 		s := []int{1, 2, 3, 4}
-		cb := func(elem int, index int) int {
+		cb := func(elem int) int {
 			return elem * 2
 		}
 
@@ -26,30 +26,27 @@ func TestFp(t *testing.T) {
 		require.Equal(t, []int{2, 4, 6, 8}, ns)
 		require.Equal(t, []int{1, 2, 3, 4}, s)
 
-		cb = func(elem int, index int) int {
+		cb2 := func(elem int, index int) int {
 			return index * elem
 		}
 
-		ns = Map(cb, s)
+		ns = MapI(cb2, s)
 		require.Equal(t, []int{0, 2, 6, 12}, ns)
 
-		cb2 := func(elem int, index int) string {
+		cb3 := func(elem int) string {
 			return fmt.Sprint(elem)
 		}
 
-		ns2 := Map(cb2, s)
+		ns2 := Map(cb3, s)
 		require.Equal(t, []string{"1", "2", "3", "4"}, ns2)
 
 		s2 := []string{"hello", "world"}
-		cb3 := func(elem string, index int) string {
-			return strings.ToUpper(elem)
-		}
 
-		ns3 := Map(cb3, s2)
+		ns3 := Map(strings.ToUpper, s2)
 		require.Equal(t, []string{"HELLO", "WORLD"}, ns3)
 
 		s3 := []person{{Name: "Amit", Age: 27}, {Name: "Rinat", Age: 27}}
-		cb4 := func(elem person, index int) string {
+		cb4 := func(elem person) string {
 			return elem.Name
 		}
 
@@ -57,18 +54,18 @@ func TestFp(t *testing.T) {
 		require.Equal(t, []string{"Amit", "Rinat"}, ns4)
 	})
 
-	t.Run("Filter", func(t *testing.T) {
+	t.Run("Filter,FilterI", func(t *testing.T) {
 		s := []int{1, 3, 5, 4}
 		cb := func(elem int, index int) bool {
 			return elem%2 == 0 || index%2 != 0
 		}
 
-		ns := Filter(cb, s)
+		ns := FilterI(cb, s)
 		require.Equal(t, []int{3, 4}, ns)
 		require.Equal(t, []int{1, 3, 5, 4}, s)
 
 		s2 := []person{{Name: "Amit", Age: 27}, {Name: "Yuval", Age: 24}}
-		cb2 := func(elem person, index int) bool {
+		cb2 := func(elem person) bool {
 			return elem.Age > 25
 		}
 
@@ -77,14 +74,22 @@ func TestFp(t *testing.T) {
 
 	})
 
-	t.Run("Reduce", func(t *testing.T) {
+	t.Run("Reduce,ReduceI", func(t *testing.T) {
 		s := []int{1, 2, 3, 4}
-		cb := func(acc int, curr int, index int) int {
+		cb := func(acc int, curr int) int {
 			return acc + curr
 		}
 
-		ns := Reduce(cb, 0, s)
-		require.Equal(t, 10, ns)
+		ns2 := Reduce(cb, 0, s)
+		require.Equal(t, 10, ns2)
+		require.Equal(t, []int{1, 2, 3, 4}, s)
+
+		cb2 := func(acc int, curr int, index int) int {
+			return acc + curr + index
+		}
+
+		ns := ReduceI(cb2, 0, s)
+		require.Equal(t, 16, ns)
 
 	})
 }
